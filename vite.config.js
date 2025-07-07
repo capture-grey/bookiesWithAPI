d42efb94c4281033dc9d22294602446baaf984e8
capture-grey <capturegrey@gmail.com>
2025-07-08 03:21:59 +0600

    yeet

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/bookiesWithAPI",
+  //base: process.env.VITE_BASE_PATH || "/bookiesWithAPI",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
